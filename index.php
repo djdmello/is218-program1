cd7f573aae30787c7fc7964a2c77b25ea2f03737
Daniel Dmello <dd255@afsconnect2.njit.edu>
2016-09-29 13:36:51 -0400

    Added some code for class

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+// This is how you print text in php
 echo 'hello world';
+// This is how you set a variable in php
+
+$myvar= 'Some Text I want to store in a variable';
+//This is how to print the variable
+echo '<br>';
+echo '$myvar';
+echo '<br>';
+echo "$myvar";
 ?>
+
 
